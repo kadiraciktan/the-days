47749279b611e884be3eeccc8ca6a20367ec084c
Kadir AÇIKTAN <k.aciktan@ulogi.com>
2023-02-17 22:45:08 +0300

    feat : added socket

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from '@the-days/database';
 
 import { AppController } from './app.controller';
+import { AppGateway } from './app.gateway';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), AppGateway],
   controllers: [AppController],
   providers: [AppService],
 })
